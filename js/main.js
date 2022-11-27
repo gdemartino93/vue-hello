361a07680360341a12dc864328a85f92c5d497a9
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-27 23:45:34 +0100

    test

--- js/main.js
+++ js/main.js
@@ -4,10 +4,18 @@ const { createApp } = Vue
         data() {
             return{
                 msgWelcome : "Ciao mondo... Im VueJs",
-               classeRosso : "red", 
-               img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png"
+                classeRosso : "red", 
+               img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png",
+               box:"box",
+
             }
         },
+        methods:{
+            saluto: function (){
+               alert("ciao era una prova")
+                
+            }
+        }
 
     }).mount("#webapp")
 
